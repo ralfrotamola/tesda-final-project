9bfb5629a5e382a644344fdeb29e6792fc013896
Ralf Kevin Rotamola <kevin.setazate@gmail.com>
2019-09-11 13:10:34 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const item = new Item;
+
+products = [
+    {title: "sabon ni koya pat1", price: "699"},
+    {title: "sabon ni koya pat2", price: "799"},
+    {title: "sabon ni koya pat2", price: "799"},
+    {title: "sabon ni koya pat2", price: "799"},
+    {title: "sabon ni koya pat3", price: "899"}
+]
+
+item.setLayouts(products);
+
+// products.forEach(product => {
+//     console.log(product.title);
+// })
+
+
+
+// setLayouts();
+// setTimeout(function(){
+
+// }, 1000);
