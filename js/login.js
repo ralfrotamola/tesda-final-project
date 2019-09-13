e9ad4fca139e57ade2e486f5f05da37daa75aa8b
Ralf Kevin Rotamola <kevin.setazate@gmail.com>
2019-09-13 14:13:02 +0800

    last commit

---
+++ js/login.js
@@ -0,0 +1,34 @@
+const login = new Login;
+
+const user = document.getElementById('user');
+const pass = document.getElementById('pass');
+const btnLogin = document.getElementById('btn-login');
+
+
+
+const loginUser = () => {
+    console.log(user.value)
+    console.log(pass.value)
+    const data = {
+        username: user.value,
+        password: pass.value,
+    }
+
+    login.loginUser('http://localhost:8080/user/login', data)
+    .then(result => {
+        if (result.success) {
+            saveToLS(result.user)
+            console.log(result.user)
+        }
+    })
+    .catch(error => console.log(error))
+}
+
+const saveToLS = (user) => {
+    if (localStorage.getItem("user") === null) {
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+    location.href = "products.html";
+}
+
+btnLogin.addEventListener('click', loginUser);
